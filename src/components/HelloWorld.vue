5e44fb385877bc6581fc7e3c2c0c4e32cf2faaa0
Ulrich Heiniger <ulrich.heiniger@nationalmuseum.ch>
2022-07-14 18:31:18 +0200

    Cleanup

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -15,33 +15,15 @@ onMounted(() => {
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
-  <h2>Persistent count: {{ count.state.value }}</h2>
-
   <div class="card">
+    <h1>{{ msg }}</h1>
+    <h2>Reload count: {{ count.state.value }}</h2>
     <button type="button" @click="count.set(0)">Reset count</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
   </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Install
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
 </template>
 
 <style scoped>
-.read-the-docs {
+.card {
   color: #888;
 }
 </style>
