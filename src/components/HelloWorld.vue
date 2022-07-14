ae4d6c0d554506f0ce49938dfdb54bfeee53f4e5
Ulrich Heiniger <ulrich.heiniger@nationalmuseum.ch>
2022-07-14 18:25:12 +0200

    Save count on reload to local storage

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,16 +1,25 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { onMounted } from 'vue'
+import useCount from '../composables/useCount';
 
 defineProps<{ msg: string }>()
 
-const count = ref(0)
+const count = useCount();
+
+onMounted(() => {
+  let currentCount = count.state.value;
+  currentCount += 1;
+  count.set(currentCount);
+});
+
 </script>
 
 <template>
   <h1>{{ msg }}</h1>
+  <h2>Persistent count: {{ count.state.value }}</h2>
 
   <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
+    <button type="button" @click="count.set(0)">Reset count</button>
     <p>
       Edit
       <code>components/HelloWorld.vue</code> to test HMR
